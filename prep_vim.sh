32ba9c2bc928e2420b3ce8e0e67906afe42397ad
Vijay Annapureddy <vijay@Vijays-MacBook-Pro.local>
2017-02-16 22:19:07 -0800

    updated prep_vim

--- prep_vim.sh
+++ prep_vim.sh
@@ -13,15 +13,33 @@ git://github.com/tpope/vim-sensible.git
 https://github.com/vim-scripts/nginx.vim
 https://github.com/scrooloose/nerdtree.git"
 
-cd ~/.vim/bundle
+# install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+mkdir -p ~/.vim/bundle
+pushd ~/.vim/bundle
 
 for f in $repos
   do
     echo "cloning repo: $f"
     git clone $f
   done
+popd
+
 # install dockerfile plugin
-cp -r dockerfile ~/.vim/bundle
+cp -r ./dockerfile ~/.vim/bundle
 
 # copy vimrc
-cp vimrc ~/.vimrc
+cp ./vimrc ~/.vimrc
+
+# git promp and completion
+curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+
+curl -o ~/.git-completion.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+
+# copy bash profile
+cp bash_git_docker_alias  ~/.bash_profile
+
+# install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
